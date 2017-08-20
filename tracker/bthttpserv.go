269908053f971ef7802b26ba3bce887a3b2a8a49
qinghe <zhangshizhuo@gmail.com>
2017-08-21 03:10:53 +0800

    解析 bt client 请求参数

--- tracker/bthttpserv.go
+++ tracker/bthttpserv.go
@@ -6,6 +6,7 @@ package tracker
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/blueskyz/uvdt/logger"
 	"github.com/blueskyz/uvdt/tracker/setting"
@@ -16,10 +17,10 @@ func BtHttpServ() {
 	// 设置 bt http server 路由
 	btHttpServMux := http.NewServeMux()
 	btHttpServMux.HandleFunc("/hello", btHelloHandler)
-	btHttpServMux.HandleFunc("/", btHandler)
+	btHttpServMux.HandleFunc("/node", btHandler)
 
 	btServ := setting.AppSetting.GetBtServ()
-	log.Info(fmt.Sprintf("Init %s:%d", btServ.Ip, btServ.Port))
+	log.Info(fmt.Sprintf("init %s:%d", btServ.Ip, btServ.Port))
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", btServ.Ip, btServ.Port), btHttpServMux)
 	if err != nil {
 		log.Err("init, " + err.Error())
@@ -27,6 +28,10 @@ func BtHttpServ() {
 }
 
 func btHelloHandler(w http.ResponseWriter, r *http.Request) {
+	// 创建日志记录器
+	log := logger.NewAgent()
+	defer log.EndLog()
+
 	fmt.Fprintf(w, "hello bt http serv")
 }
 
@@ -35,6 +40,30 @@ func btHandler(w http.ResponseWriter, r *http.Request) {
 	log := logger.NewAgent()
 	defer log.EndLog()
 
-	log.Info("This is Info 1")
-	fmt.Fprintf(w, fmt.Sprintf("bt http serv %s", r.RequestURI))
+	log.Info(r.RequestURI)
+	// 解析 bt 参数
+	values := r.URL.Query()
+	if len(values) == 0 {
+		log.Err("arguments err")
+	}
+
+	info_hash := values.Get("info_hash")
+	if len(info_hash) == 0 {
+		log.Err("info_hash is empty")
+	}
+
+	compact := values.Get("compact")
+
+	peer_id := values.Get("peer_id")
+	if len(peer_id) == 0 {
+		log.Err("peer_id is empty")
+	}
+
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+	port := values.Get("port")
+	if len(port) == 0 {
+		log.Err("port is empty")
+	}
+	log.Info(fmt.Sprintf("info_hash: %s, compact: %s, peer_id: %s, ip: %s, port: %s",
+		info_hash, compact, peer_id, ip, port))
 }
