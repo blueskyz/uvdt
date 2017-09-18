3d6cb30e5cd5bafedc59466c2414023fb11093fe
qinghe <zhangshizhuo@gmail.com>
2017-09-19 00:53:41 +0800

    添加 json 格式bt文件

--- node-tool/createtorrentfile.go
+++ node-tool/createtorrentfile.go
@@ -2,6 +2,7 @@ package nodetool
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/blueskyz/uvdt/node-tool/setting"
@@ -28,31 +29,53 @@ func (creator *CreatorTorrent) ScanPath() ([]string, error) {
 				return []string{}, err
 			}
 			if !fileInfo.IsDir() {
-				md5sum, _ := creator.calcFileMd5(
+				file_md5sum, fragments_md5sum, err := creator.calcFileMd5(
 					path.Join(appSetting.GetAbResPath(), fileInfo.Name()))
+
+				if err != nil {
+					return []string{}, err
+				}
+
 				log.Printf("File info: name=%s, size=%d, md5sum=%s, mtime=%s\n",
 					fileInfo.Name(),
 					fileInfo.Size(),
-					md5sum,
+					file_md5sum,
 					fileInfo.ModTime().Format(time.RFC3339))
+
+				c := make(map[string]interface{})
+				c["file_name"] = fileInfo.Name()
+				c["file_size"] = fileInfo.Size()
+				c["file_md5"] = file_md5sum
+				c["mtime"] = fileInfo.ModTime().UnixNano()
+				c["file_fragments"] = fragments_md5sum
+				torrent, err := json.Marshal(c)
+				if err != nil {
+					return []string{}, err
+				}
+				log.Printf("%s", torrent)
 			}
 		}
 	}
 	return []string{}, nil
 }
 
-func (creator *CreatorTorrent) calcFileMd5(filePath string) (string, error) {
+func (creator *CreatorTorrent) calcFileMd5(filePath string) (string,
+	[]string,
+	error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 	defer f.Close()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		return "", err
+		return "", nil, err
 	}
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
+	file_md5sum := fmt.Sprintf("%x", h.Sum(nil))
+	fragments_md5sum := []string{}
+
+	return file_md5sum, fragments_md5sum, nil
 }
 
 func (creator *CreatorTorrent) File2TorrentFile(filePath string) error {
